cbef5a4c41b8a46c63be2a35f76bcbd3bfe96a6f
BHC-Chicken <tij05078@gmail.com>
2026-02-01 22:14:05 +0900

    Refactor: 모호성 제거를 위한 변수 이름 변경

--- knock-backend/core/core-api/src/main/java/com/knock/core/api/controller/v1/request/ReviewCreateRequest.java
+++ knock-backend/core/core-api/src/main/java/com/knock/core/api/controller/v1/request/ReviewCreateRequest.java
@@ -1,4 +1,4 @@
 package com.knock.core.api.controller.v1.request;
 
-public record ReviewCreateRequest(Long id, String content, int score) {
+public record ReviewCreateRequest(Long itemId, String content, int score) {
 }
--- knock-backend/core/core-api/src/main/java/com/knock/core/domain/review/dto/request/ReviewCreateData.java
+++ knock-backend/core/core-api/src/main/java/com/knock/core/domain/review/dto/request/ReviewCreateData.java
@@ -4,6 +4,6 @@ import com.knock.core.api.controller.v1.request.ReviewCreateRequest;
 
 public record ReviewCreateData(Long itemId, String content, int score) {
 	public static ReviewCreateData from(ReviewCreateRequest request) {
-		return new ReviewCreateData(request.id(), request.content(), request.score());
+		return new ReviewCreateData(request.itemId(), request.content(), request.score());
 	}
 }
